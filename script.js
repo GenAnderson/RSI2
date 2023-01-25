882c150d4d417dfed1e03140951dc86f54c7c57e
Genevieve Anderson <114549536+GenAnderson@users.noreply.github.com>
2023-01-25 12:55:58 -1000

    fixed menu layout

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ const nextImageDelay = 3000;
 
 let currentImageCounter = 0;
 
-// slideshowImages[currentImageCounter].style.display = "block";
 slideshowImages[currentImageCounter].style.opacity = 1;
 
 setInterval(nextImage, nextImageDelay);
